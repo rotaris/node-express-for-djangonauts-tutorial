d45a0c844d6bf6a1314e2c320e5baf2968ed9214
Arun Neelakandan <arun.neelakandan@gmail.com>
2013-09-01 16:21:57 +1000

    Added some views including pollIndex() and detail(). Also added a 404 handler with custom template and moved the 'view' functions into a separate file.

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var express = require('express')
   , user = require('./routes/user')
   , http = require('http')
   , mongoose = require('mongoose')
+  , pollsdb = require('./models/db')
   , path = require('path');
 
 var app = express();
@@ -22,16 +23,38 @@ app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+// Error Handling
+// As per "How do you handle 404s?" on http://expressjs.com/faq.html
+app.use(function(req, res){
+  res.status(404).render('404');
+});
 
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-mongoose.connect('mongodb://localhost/pollsdb');
-
+// A couple of example routes that were
+// created when the app skeleton was created
 app.get('/', routes.index);
 app.get('/users', user.list);
+
+// Our routes
+app.get('/polls', routes.pollIndex);
+app.get('/polls/:poll_id', routes.detail);
+app.get('/polls/:poll_id/results', routes.results);
+app.get('/polls/:poll_id/vote', routes.vote);
+app.post('/polls/:poll_id/vote', routes.vote);
+
+// Example regex route
+// Show shoes between certain sizes
+// Example valid URL 1: /items/shoes/sizes/8-13
+// Example valid URL 2: /items/shoes/sizes/10-13
+app.get(/^\/items\/shoes\/sizes\/(\d{0,2})-(\d{1,2})$/, function(req, res){
+  var min_size = req.params[0];
+  var max_size = req.params[1];
+  res.send('Showing shoes between sizes ' + min_size + ' to ' + max_size);
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,14 @@
+var mongoose = require('mongoose');
+var pollsdb = require('../models/db');
+
+mongoose.connect('mongodb://localhost/pollsdb');
+
+var conn = mongoose.connection
+conn.on('error', console.error.bind(console, 'Database connection error:'));
+conn.once('open', function callback () { console.log("Hey handsome. Database connected.") })
+
+var Poll = conn.model("Poll", pollsdb.pollSchema)
+
 
 /*
  * GET home page.
@@ -6,3 +17,57 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+exports.pollIndex = function(req, res, next) {
+  Poll.find().sort('-pubDate').limit(5).exec(function(err, polls) {
+    if (err)
+      next("Uh oh! Something went wrong when fetching polls from the database.");
+    else {
+      var questions = new Array();
+      for (var i = 0; i < polls.length; i++) {
+        questions.push(polls[i].question);
+      }
+      // Make a comma-separated string of questions
+      result = questions.join(", ");
+      //res.send(result);
+      res.render('pollIndex', { title: "Latest Polls", latest_poll_list: polls });
+    }
+  }); 
+}
+
+exports.detail = function(req, res, next) {
+  var poll_id = req.params.poll_id;
+  // Check if the ID provided is an ObjectID used by MongoDB (used by default).
+  // Could also do this matching in the routes specification.
+  // Refer to: http://stackoverflow.com/a/14942113/977931
+  if (poll_id.match(/^[0-9a-fA-F]{24}$/)) {
+    // findById() is akin to Django's get(pk=poll_id)
+    Poll.findById(poll_id).exec(function(err, poll) { 
+      if (err) {
+        console.log(err);
+        // Return a 500 Internal Server Error
+        next("Uh oh! Something went wrong when fetching a poll from the database.");
+      }
+      else if (poll) {
+        res.render('pollDetail', { poll: poll });
+      }
+      else {
+        // Pass it along, nothing found (should deliver a 404)
+        next();
+      }
+    });
+  }
+  else
+    // Pass it along, nothing found (should deliver a 404)
+    next();
+}
+
+exports.results = function(req, res) {
+    poll_id = req.params.poll_id
+    res.send("You're looking at the results of poll " + poll_id);
+}
+
+exports.vote = function(req, res) {
+    poll_id = req.params.poll_id
+    res.send("You're voting on poll " +  poll_id);
+}
